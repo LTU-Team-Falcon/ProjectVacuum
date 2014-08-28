e1cf63fa0cc87d7a967d9ac37d269cba801a3d3a
JmatherLTU <jmather@ltu.edu>
2014-08-28 17:54:33 -0400

    fixed materials for short house blocks and updated grass texture.

---
+++ Human Lifestyle Emulator/Assets/_Scripts/PowerUpLightFlash.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var FlashingLight : Light;
+FlashingLight.enabled = false;
+
+
+function FixedUpdate () {
+
+var RandomNumber = Random.value;
+
+	if (RandomNumber<=.95){
+	FlashingLight.enabled=true;
+
+	}
+	else FlashingLight.enabled=false;
+
+}
